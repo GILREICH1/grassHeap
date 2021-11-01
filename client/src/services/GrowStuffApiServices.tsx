4c60e886b31e83035820d2e274585657f6ddcc10
gilreich1 <gilreich12@gmail.com>
2021-11-01 21:06:05 +0200

    tsx fix

--- client/src/services/GrowStuffApiServices.tsx
+++ client/src/services/GrowStuffApiServices.tsx
@@ -1,22 +1,14 @@
 import { SERVER_URL as base_url } from '../utils/config';
 import { Plant } from '../common/types';
 
-export const getAllPlants = async (): Promise<Plant[] | unknown> => {
-  try {
-    const JSONPlants = await fetch(`${base_url}/plants`);
-    const plants = await JSONPlants.json();
-    return plants;
-  } catch (err) {
-    return err;
-  }
+export const getAllPlants = async (): Promise<Plant[]> => {
+  const JSONPlants = await fetch(`${base_url}/plants`);
+  const plants = await JSONPlants.json();
+  return plants;
 };
 
-export const getPlantByName = async (name = ''): Promise<Plant | unknown> => {
-  try {
-    const JSONPlant = await fetch(`${base_url}/crops/${name}`);
-    const plant = await JSONPlant.json();
-    return plant;
-  } catch (err) {
-    return err;
-  }
+export const getPlantByName = async (name = ''): Promise<Plant> => {
+  const JSONPlant = await fetch(`${base_url}/crops/${name}`);
+  const plant = await JSONPlant.json();
+  return plant;
 };
