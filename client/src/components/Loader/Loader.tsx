a067011e436fff148167414257c6859154a04487
gilreich1 <gilreich12@gmail.com>
2021-11-01 20:31:36 +0200

    cleanup

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import './Loader.css';
 function Loader(): JSX.Element {
   return (
     <div className="App__loading">
-      <h3>Loading DOM trees and DOM vegetables...</h3>
+      <h3>Watering...</h3>
       <img
         className="daisies"
         src="https://uploads-ssl.webflow.com/5ea82bf973106e1765b18a5f/5fd0d4198fecb12496f3bbdb_tumbs-up-4_1.gif"
