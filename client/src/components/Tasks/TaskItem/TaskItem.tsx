6ce004d724ee48a3105176cac402ed731667d60f
gilreich1 <gilreich12@gmail.com>
2021-11-07 13:39:40 +0200

    remove disabled button

--- client/src/components/Tasks/TaskItem/TaskItem.tsx
+++ client/src/components/Tasks/TaskItem/TaskItem.tsx
@@ -12,14 +12,6 @@ function TaskItem({ task, deleteThisTask }: TaskItemProps): JSX.Element {
   const taskIcon = `https://www.growstuff.org/crops/${task.crop}.svg`;
   const backUpIcon =
     'https://www.growstuff.org/assets/icons/planting-ce51a46e4a6edd740221f4a98f2e630a944e30ca040b9000d25179c8f5bc17e8.svg';
-  const disabledButton: JSX.Element = (
-    <div>
-      <button className={styles.disabled} disabled>
-        X
-      </button>
-      <span className={styles.tooltiptext}>cannot delete default tasks</span>
-    </div>
-  );
 
   return (
     <div className={styles.TaskItem}>
@@ -34,9 +26,7 @@ function TaskItem({ task, deleteThisTask }: TaskItemProps): JSX.Element {
       <p>{task.task}</p>
       {task.userCreated ? (
         <button onClick={() => deleteThisTask(task._id!)}>X</button>
-      ) : (
-        disabledButton
-      )}
+      ) : null}
     </div>
   );
 }
