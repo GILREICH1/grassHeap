869ad3494f61a4f21e88ba9c3fd5a1e44c516bb2
gilreich1 <gilreich12@gmail.com>
2021-11-07 12:31:31 +0200

    loader changes

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import './Loader.css';
 
 function Loader(): JSX.Element {
+  const [i, seti] = useState(0);
+  const ellipses = ['', '.', '..', '...'];
+
+  useEffect(() => {
+    let count = 0;
+    const id = setInterval(() => {
+      seti(count % ellipses.length);
+      count++;
+    }, 700);
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
   return (
     <div className="App__loading">
-      <h3>Watering...</h3>
+      <h1>{`Watering${ellipses[i]}`}</h1>
       <img
         className="daisies"
         src="https://uploads-ssl.webflow.com/5ea82bf973106e1765b18a5f/5fd0d4198fecb12496f3bbdb_tumbs-up-4_1.gif"
