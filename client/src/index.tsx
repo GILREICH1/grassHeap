c6e1f670c9c2a43f44737237ae1bba49a2679643
gilreich1 <gilreich12@gmail.com>
2021-11-11 19:39:48 +0200

    added QueryClient

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const reactQueryClient = new QueryClient();
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={reactQueryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
