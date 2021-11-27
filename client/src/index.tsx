4f005314351aedf3e986a2fb30f41bffec081767
gilreich1 <gilreich12@gmail.com>
2021-11-27 18:47:55 +0200

    added dev tools

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const reactQueryClient = new QueryClient();
@@ -11,6 +12,7 @@ ReactDOM.render(
   <BrowserRouter>
     <QueryClientProvider client={reactQueryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </BrowserRouter>,
   document.getElementById('root'),
