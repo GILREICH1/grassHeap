ff6626a8583f35cb6e8d5b66318cb18b44417507
Viktor Ricchiuto <ricchiuto.viktor@gmail.com>
2021-12-17 07:48:26 +0100

    improve .env file usage

--- server/models/database.js
+++ server/models/database.js
@@ -1,21 +1,28 @@
 const DB_USER = process.env.DB_USER;
 const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
 
-const DB_NAME =
-  process.env.NODE_ENV === "test"
-    ? process.env.DATABASE_TEST
-    : process.env.DATABASE;
+function makeConnectionURI() {
+  const IS_TEST = process.env.NODE_ENV === 'test';
+  const DB_NAME = IS_TEST ? process.env.DATABASE_TEST : process.env.DATABASE;
 
-let connectionURI;
-if (DB_USER && DB_USER_PASSWORD && process.env.NODE_ENV !== "test")
-  connectionURI = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.kbmrd.mongodb.net/grassHeap?retryWrites=true&w=majority`;
-else connectionURI = `mongodb://localhost:27017/${DB_NAME}`;
+  if (!DB_USER || !DB_USER_PASSWORD || IS_TEST) {
+    return `mongodb://localhost:27017/${DB_NAME}`;
+  }
 
-const mongoose = require("mongoose");
-mongoose.connect(connectionURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  return `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.9g4e8.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+}
+
+const connectionURI = makeConnectionURI();
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(connectionURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connected to DB'))
+  .catch((reason) => console.log('Failed to connect to DB', reason));
 
 const db = mongoose.connection;
 
