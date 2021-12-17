1f772143f45caf97f519ff9448193b30b43d0a81
Viktor Ricchiuto <ricchiuto.viktor@gmail.com>
2021-12-17 08:08:53 +0100

    server: add prettier config

---
+++ server/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  singleQuote: false,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  quoteProps: 'consistent',
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+};
