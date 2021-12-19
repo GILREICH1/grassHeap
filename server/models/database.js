071a38d1f916d64a400c20c8e2a16a34904ab62d
Viktor Ricchiuto <ricchiuto.viktor@gmail.com>
2021-12-19 10:59:47 +0100

    server: add db host to .env file

--- server/models/database.js
+++ server/models/database.js
@@ -1,3 +1,4 @@
+const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
 
@@ -5,11 +6,11 @@ function makeConnectionURI() {
   const IS_TEST = process.env.NODE_ENV === 'test';
   const DB_NAME = IS_TEST ? process.env.DATABASE_TEST : process.env.DATABASE;
 
-  if (!DB_USER || !DB_USER_PASSWORD || IS_TEST) {
+  if (!DB_HOST || !DB_USER || !DB_USER_PASSWORD || IS_TEST) {
     return `mongodb://localhost:27017/${DB_NAME}`;
   }
 
-  return `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.9g4e8.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+  return `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 }
 
 const connectionURI = makeConnectionURI();
@@ -19,10 +20,10 @@ const mongoose = require('mongoose');
 mongoose
   .connect(connectionURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log('connected to DB'))
-  .catch((reason) => console.log('Failed to connect to DB', reason));
+  .catch(reason => console.log('Failed to connect to DB', reason));
 
 const db = mongoose.connection;
 
