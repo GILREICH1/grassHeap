30d97fbf46c4ef2844bd95b47eba401f45799b98
gilreich1 <gilreich12@gmail.com>
2023-02-17 17:57:04 +0200

    add joy UI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,13 +5,16 @@ import './index.css';
 import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import App from './components/App/App';
 import UserContext from './components/Authentication/UserContext';
+import { CssVarsProvider } from '@mui/joy';
 
 ReactDOM.render(
   <BrowserRouter>
     <Auth0ProviderWithHistory>
-      <UserContext>
-        <App />
-      </UserContext>
+      <CssVarsProvider>
+        <UserContext>
+          <App />
+        </UserContext>
+      </CssVarsProvider>
     </Auth0ProviderWithHistory>
   </BrowserRouter>,
   document.getElementById('root'),
