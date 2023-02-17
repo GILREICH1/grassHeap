a475480d7c92bf99a7699e07cc12e211d3deb8af
gilreich1 <gilreich12@gmail.com>
2023-02-17 17:57:19 +0200

    initial add joy ui to taskform

--- client/src/components/Tasks/AddTaskForm/AddTaskForm.tsx
+++ client/src/components/Tasks/AddTaskForm/AddTaskForm.tsx
@@ -6,6 +6,12 @@ import { plantsContext } from '../../App/App';
 import { userContxt } from '../../Authentication/UserContext';
 import { useAuth0 } from '@auth0/auth0-react';
 import { v4 as uuid } from 'uuid';
+import Input from '@mui/joy/Input';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Button from '@mui/joy/Button';
+import CloseRounded from '@mui/icons-material/CloseRounded';
+import IconButton from '@mui/joy/IconButton';
 
 interface AddTaskFormProps {
   month: string;
@@ -14,6 +20,7 @@ interface AddTaskFormProps {
 
 function AddTaskForm({ month, addNewTask }: AddTaskFormProps): JSX.Element {
   const [crop, setCrop] = useState<string>('');
+  const action = React.useRef(null);
   const [task, setTask] = useState<string>('');
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
   const { plants } = useContext(plantsContext);
@@ -40,34 +47,59 @@ function AddTaskForm({ month, addNewTask }: AddTaskFormProps): JSX.Element {
 
   return (
     <>
-      <form className={styles.submitForm} onSubmit={submitHandler}>
-        <h3>Add New Task</h3>
-        <div className={styles['form__group']}>
-          <input
-            className={styles['form__field']}
-            required
-            value={task}
-            onChange={e => setTask(e.target.value)}
-          />
-          <label className={styles['form__label']}>Task</label>
-        </div>
-        <div className={`${styles['form__group']}`}>
-          <input
-            list="plant-list"
-            className={styles['form__field']}
-            required
-            value={crop}
-            onChange={e => setCrop(e.target.value.toLowerCase())}
-          />
-          <label className={styles['form__label']}>Crop</label>
-          <datalist id="plant-list">
-            {plants.map(plant => (
-              <option key={plant.id} value={plant.name} />
-            ))}
-          </datalist>
-        </div>
-        <input className={styles.submit} type="submit" value="Submit" />
-      </form>
+      <h3>Add New Task</h3>
+      <label id="task-label">Task</label>
+      <Input
+        value={task}
+        onChange={e => setTask(e.target.value)}
+        color="success"
+        placeholder="water"
+        size="md"
+        required
+      />
+      <label id="crop-input-label">Crop</label>
+      <Select
+        action={action}
+        // @ts-expect-error there is an error here
+        onChange={(_e, v) => setCrop(v)}
+        color="success"
+        placeholder="Choose one..."
+        value={crop}
+        {...(crop && {
+          // display the button and remove select indicator
+          // when user has selected a value
+          endDecorator: (
+            <IconButton
+              size="sm"
+              variant="plain"
+              color="neutral"
+              onMouseDown={event => {
+                // don't open the popup when clicking on this button
+                event.stopPropagation();
+              }}
+              onClick={() => {
+                setCrop('');
+              }}>
+              <CloseRounded />
+            </IconButton>
+          ),
+          indicator: null,
+        })}>
+        {plants.map(plant => {
+          return (
+            <Option key={plant.id} value={plant.name}>
+              {plant.name}
+            </Option>
+          );
+        })}
+      </Select>
+      <Button
+        disabled={!(crop && task)}
+        color={crop && task ? 'success' : 'danger'}
+        variant="solid"
+        onClick={submitHandler}>
+        Submit
+      </Button>
     </>
   );
 }
