91e6323d6b8db3fced49ea3c0ad603caa08befd0
gilreich1 <gilreich12@gmail.com>
2023-02-17 15:01:03 +0200

    cleanup

--- client/src/utils/months.tsx
+++ client/src/utils/months.tsx
@@ -12,19 +12,3 @@ export const months: string[] = [
   'November',
   'December',
 ];
-
-// LEGACY;
-// export const bgColor: { [key: string]: string } = {
-//   January: '#382C05',
-//   February: '#5E4A08',
-//   March: '#83670B',
-//   April: '#AA850E',
-//   May: '#D0A311',
-//   June: '#EDBC1D',
-//   July: '#F0C742',
-//   August: '#F3D268',
-//   September: '#F6DE8E',
-//   October: '#F7E3A1',
-//   November: '#F8E8B4',
-//   December: '#FAEEC7',
-// };
