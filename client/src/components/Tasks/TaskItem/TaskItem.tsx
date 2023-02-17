dc659b10dfb36d80ccd7fc84f3dbef620fb10e2f
gilreich1 <gilreich12@gmail.com>
2023-02-17 19:04:15 +0200

    restyle taskItem with MUI

--- client/src/components/Tasks/TaskItem/TaskItem.tsx
+++ client/src/components/Tasks/TaskItem/TaskItem.tsx
@@ -1,5 +1,9 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+import Button from '@mui/joy/Button';
+import Chip from '@mui/joy/Chip';
+
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+
 import { Task } from '../../../common/types';
 import styles from './TaskItem.module.scss';
 
@@ -14,27 +18,40 @@ function TaskItem({ task, deleteThisTask }: TaskItemProps): JSX.Element {
     'https://www.growstuff.org/assets/icons/planting-ce51a46e4a6edd740221f4a98f2e630a944e30ca040b9000d25179c8f5bc17e8.svg';
 
   return (
-    <div className={styles.TaskItem}>
-      <div className={styles.div2}>
-        <h3>
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        p: '8px',
+        outline: 'solid var(--joy-palette-success-200); border-radius: 0.5rem;',
+      }}
+      spacing={2}>
+      <Stack
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="flex-start">
+        <Typography level="h6">
           {task.crop}{' '}
-          <img
-            className={styles.taskItem_crop_icon}
-            src={taskIcon}
-            onError={e => ((e.target as HTMLImageElement).src = backUpIcon)}
-          />
-        </h3>
-      </div>
-      <div className={styles.div3}>
-        <p>{task.task}</p>
-      </div>
-      <div className={styles.div1}>
-        {task.userCreated && <div className={styles.bar}></div>}
-        {task.userCreated && (
-          <button onClick={() => deleteThisTask(task._id)}>X</button>
-        )}
-      </div>
-    </div>
+          <Chip variant="plain">
+            <img
+              className={styles.taskItem_crop_icon}
+              src={taskIcon}
+              onError={e => ((e.target as HTMLImageElement).src = backUpIcon)}
+            />
+          </Chip>
+        </Typography>
+        <Typography level="body1">{task.task}</Typography>
+      </Stack>
+      {task.userCreated && (
+        <Button
+          size="sm"
+          color="danger"
+          onClick={() => deleteThisTask(task._id)}>
+          X
+        </Button>
+      )}
+    </Stack>
   );
 }
 
