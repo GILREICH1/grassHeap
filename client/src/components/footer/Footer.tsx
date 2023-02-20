57b5a0b5712743d09d5be5ee20788f6d1439ab90
gilreich1 <gilreich12@gmail.com>
2023-02-20 20:17:23 +0200

    sort footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,16 +1,26 @@
+import { Link } from '@mui/joy';
+import Typography from '@mui/joy/Typography';
 import styles from './Footer.module.scss';
 function Footer(): JSX.Element {
   return (
     <footer className={styles['footer']}>
-      A Project by
-      <a href="https://github.com/GILREICH1" target="_blank" rel="noreferrer">
-        {' '}
-        Gil Reich{' '}
-      </a>
-      with thanks to the{' '}
-      <a href="https://github.com/Growstuff/growstuff/wiki/API-Version-0">
-        GrowStuff API
-      </a>
+      <Typography>
+        A Project by{' '}
+        <Link
+          href="https://github.com/GILREICH1"
+          target="_blank"
+          rel="noreferrer">
+          Gil Reich{' '}
+        </Link>
+        {' - '}
+        with thanks to the{' '}
+        <Link
+          rel="noreferrer"
+          target="_blank"
+          href="https://github.com/Growstuff/growstuff/wiki/API-Version-0">
+          GrowStuff API
+        </Link>
+      </Typography>
     </footer>
   );
 }
