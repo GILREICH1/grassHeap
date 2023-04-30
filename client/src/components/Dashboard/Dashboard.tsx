db6d40091185ec7dfa1f2a9d227776975069f074
gilreich1 <gilreich12@gmail.com>
2023-04-30 17:08:42 +0300

    cleanup

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MyPlantsList from '../MyPlants/MyPlantsList';
 import Tasks from '../Tasks/Tasks/Tasks';
-import Weather from '../Weather/Weather';
+// import Weather from '../Weather/Weather';
 import styles from './Dashboard.module.scss';
 
 function Dashboard(): JSX.Element {
