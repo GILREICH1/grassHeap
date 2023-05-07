796bfe7f699dd5e60b066fb22bb318c563480807
gilreich1 <gilreich12@gmail.com>
2023-05-07 16:47:22 +0300

    remove weather from client

--- client/src/components/Weather/CurrentWeather/CurrentWeather.tsx
+++ client/src/components/Weather/CurrentWeather/CurrentWeather.tsx
@@ -1,6 +1,5 @@
 import { APIWeather } from '../../../common/types';
 import styles from './CurrentWeather.module.scss';
-import { useWeatherGif } from '../useWeatherGif';
 
 interface Props {
   weather: APIWeather;
@@ -10,12 +9,12 @@ interface Props {
  * Left side of the weather widget.
  */
 function CurrentWeather({ weather }: Props): JSX.Element {
-  const gifPath = useWeatherGif(weather.weather[0]?.description || '');
-
   return (
     <div className={styles['container']}>
       <h2>{'Today'}</h2>
-      <img className={styles['gif']} src={gifPath}></img>
+      <img
+        className={styles['icon']}
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
       <div className={styles['numericData']}>
         <h3>{weather.main && Math.round(weather.main?.temp)} °C</h3>
         <h3>{weather.weather[0].description}</h3>
